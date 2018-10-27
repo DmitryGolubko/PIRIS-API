14e12164012572a86474c3a19bb33e929435b49e
Dmitry Golubko <golubko97@gmail.com>
2018-10-27 13:37:57 +0300

    CORS config fix

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module PIRIS
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
+        resource '*', headers: :any, methods: %i[get post options put patch delete]
       end
     end
   end
